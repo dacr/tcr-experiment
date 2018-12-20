32b4dc681f327edc910afdb73548247af188641f
David Crosson <david.crosson@orange.com>
2018-12-20 21:12:48 +0100

    working

--- src/main/scala/Greetings.scala
+++ src/main/scala/Greetings.scala
@@ -4,6 +4,9 @@ object Greetings {
       case null::Nil => s"Hello, my friend."
       case name::Nil if name.forall(_.isUpper) => s"HELLO $name!"
       case name::Nil => s"Hello, $name."
+      case names if names.exists(_.forall(_.isUpper)) =>
+        val (shouting, notshouting) = names.partition(_.forall(_.isUpper))
+        sayHello(notshouting:_*)+" AND "+sayHello(shouting:_*)
       case names => s"""Hello, ${names.init.mkString(", ")} and ${names.last}."""
     }
   }
